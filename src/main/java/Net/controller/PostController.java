b52d42d0d8d0467cd20cb6482d405a3151a941a2
SKishchenko <userg@yandex.ru>
2024-10-24 23:34:33 +0300

    Update import

--- src/main/java/Net/controller/PostController.java
+++ src/main/java/Net/controller/PostController.java
@@ -5,15 +5,11 @@ import com.google.gson.Gson;
 import Net.model.Post;
 import Net.service.PostService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Reader;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
